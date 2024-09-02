0a109027ca8b3f0c3baf29f2bc2eafde095b17d7
Jay <jay.kim@theway.dev>
2024-09-02 23:18:40 +0900

    Create a table for patients

--- main.go
+++ main.go
@@ -3,8 +3,33 @@ Copyright © 2024 JK <jay.luke.kim@gmail.com>
 */
 package main
 
-import "github.com/jaylkim/ups/cmd"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/jaylkim/ups/cmd"
+	_ "github.com/mattn/go-sqlite3"
+)
 
 func main() {
+	database, err := sql.Open("sqlite3", "./db/example.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer database.Close()
+
+	// Create a table
+	statement, err := database.Prepare(`CREATE TABLE IF NOT EXISTS patient (
+		pid INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		age INTEGER NOT NULL
+	)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	statement.Exec()
+	fmt.Println("Table patient successfully created!")
+
 	cmd.Execute()
 }
