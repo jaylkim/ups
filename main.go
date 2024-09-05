5b88847a389247001dd82bd69d41a205fc9b5ad4
Jay <jay.kim@theway.dev>
2024-09-05 23:00:17 +0900

    Add init() function so that it creates a patient table before running the main module

--- main.go
+++ main.go
@@ -12,24 +12,29 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var database *sql.DB
+
 func main() {
-	database, err := sql.Open("sqlite3", "./db/example.db")
+	defer database.Close()
+	cmd.Execute()
+}
+
+func init() {
+	var err error
+	database, err = sql.Open("sqlite3", "./db/example.db")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer database.Close()
-
 	// Create a table
 	statement, err := database.Prepare(`CREATE TABLE IF NOT EXISTS patient (
 		pid INTEGER PRIMARY KEY AUTOINCREMENT,
 		name TEXT NOT NULL,
-		age INTEGER NOT NULL
+		age INTEGER NOT NULL,
+
 	)`)
 	if err != nil {
 		log.Fatal(err)
 	}
 	statement.Exec()
 	fmt.Println("Table patient successfully created!")
-
-	cmd.Execute()
 }
