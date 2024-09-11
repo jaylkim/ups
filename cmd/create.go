31ea7340174bd4461adee0c378333cb11781de0e
Jay <jay.kim@theway.dev>
2024-09-11 20:16:43 +0900

    I have no idea what I've done

--- cmd/create.go
+++ cmd/create.go
@@ -4,12 +4,16 @@ Copyright © 2024 JK <jay.luke.kim@gmail.com>
 package cmd
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 
 	"github.com/spf13/cobra"
 )
 
 var flagPatient bool
+
+var database *sql.DB
 
 // createCmd represents the create command
 var createCmd = &cobra.Command{
@@ -23,14 +27,39 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if flagPatient {
-			fmt.Println("Creating a patient")
+			createPatient(database, args[0], args[1], args[2])
+			fmt.Println("A patient is created.")
 		} else {
 			fmt.Println("Creating other things")
 		}
 	},
 }
 
+func createPatient(db *sql.DB, name string, age string, sex string) (int64, error) {
+	statement, err := db.Prepare("INSERT INTO patient (name, age, sex) VALUES (?, ?, ?)")
+	if err != nil {
+		return 0, err
+	}
+	defer statement.Close()
+
+	_, err = statement.Exec(
+		name,
+		age,
+		sex,
+	)
+	if err != nil {
+		return 0, err
+	}
+
+	return 1, nil
+}
+
 func init() {
+	var err error
+	database, err = sql.Open("sqlite3", "./db/example.db")
+	if err != nil {
+		log.Fatal(err)
+	}
 	rootCmd.AddCommand(createCmd)
 	createCmd.Flags().BoolVarP(&flagPatient, "patient", "p", false, "Create a patient")
 }
